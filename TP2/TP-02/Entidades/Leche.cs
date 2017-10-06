d24e3daf223201ad206e7e568620b7a378e11f32
perez-artaso <perez.artaso@gmail.com>
2017-10-06 01:03:32 -0300

    Add files via upload

---
+++ TP2/TP-02/Entidades/Leche.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2017
+{
+    public class Leche : Producto
+    {
+        
+        ETipo _tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string codigo, ConsoleColor color)
+            : base(codigo, marca, color)
+        {
+            _tipo = ETipo.Entera;
+        }
+
+        public Leche(EMarca marca, string codigo, ConsoleColor color, ETipo tipo)
+            : base(codigo, marca, color)
+        {
+            this._tipo = tipo;
+
+        }
+ 
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine(string.Format("CALORIAS : {0}", this.CantidadCalorias));
+            sb.AppendLine("TIPO : " + this._tipo);
+            sb.AppendLine(" ");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        public enum ETipo
+        {
+            Entera,
+            Descremada
+        };
+    }
+}
---
+++ TP2/TP-02/Entidades/Producto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    /// <summary>
+    /// La clase Producto será abstracta, evitando que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+
+        EMarca _marca;
+        string _codigoDeBarras;
+        ConsoleColor _colorPrimarioEmpaque;
+
+        public Producto(string codigoDeBarras, EMarca marca, ConsoleColor color)
+        {
+            _marca = marca;
+            _codigoDeBarras = codigoDeBarras;
+            _colorPrimarioEmpaque = color;
+        }
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de ruedas del vehículo (?)
+        /// </summary>
+        public abstract short CantidadCalorias { get; }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("CODIGO DE BARRAS: {0}", p._codigoDeBarras));
+            sb.AppendLine(string.Format("MARCA          : {0}", p._marca.ToString()));
+            sb.AppendLine(string.Format("COLOR EMPAQUE  : {0}", p._colorPrimarioEmpaque.ToString()));
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+
+        public enum EMarca
+        {
+            Serenisima,
+            Campagnola,
+            Arcor,
+            Ilolay,
+            Sancor,
+            Pepsico
+        };
+    }
+}

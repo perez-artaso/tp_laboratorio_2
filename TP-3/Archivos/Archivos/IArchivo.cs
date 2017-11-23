68d12b7a0cb8df543ab0eb741877a21f1f439bd3
unknown <perez.artaso@gmail.com>
2017-11-23 04:03:20 -0300

    TP3

---
+++ TP-3/Archivos/Archivos/IArchivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Archivos
+{
+    interface IArchivo<T>
+    {
+        bool guardar(string archivo, T datos);
+
+        bool leer(string archivo, out T datos);
+    }
+}
---
+++ TP-3/Excepciones/Excepciones/NacionalidadInvalidaException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class NacionalidadInvalidaException : Exception
+    {
+        static string mensaje = "La Nacionalidad No Coincide Con El DNI";
+
+        public NacionalidadInvalidaException()
+            : base(mensaje)
+        {
+        }
+
+        public NacionalidadInvalidaException(string nuevoMensaje)
+            : base(nuevoMensaje)
+        {
+        }
+    }
+}
